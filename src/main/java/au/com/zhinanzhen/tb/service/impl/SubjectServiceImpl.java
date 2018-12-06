17890045c1b1aae74564fbb2c94483ccc0d64544
Larry <larry7696@gmail.com>
2018-12-07 04:50:15 +0800

    coding

--- src/main/java/au/com/zhinanzhen/tb/service/impl/SubjectServiceImpl.java
+++ src/main/java/au/com/zhinanzhen/tb/service/impl/SubjectServiceImpl.java
@@ -352,9 +352,11 @@ public class SubjectServiceImpl extends BaseService implements SubjectService {
 	@Override
 	public int newChildSubjectId(int pId) throws ServiceException {
 		SubjectDO subjectDo = subjectDAO.selectById(pId);
+		int parentId = subjectDo.getId();
 		subjectDo.setId(-1);
 		if(SubjectTypeEnum.INDIE.name().equalsIgnoreCase(subjectDo.getType())) {
 			subjectDo.setType(SubjectTypeEnum.CHILD.name());
+			subjectDo.setParentId(parentId);
 			subjectDAO.addSubject(subjectDo);
 			return subjectDo.getId();
 		} else
